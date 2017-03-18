5a5b107bfdc32ba53d797766d0374e2355f4f2b1
Samuel Phillips <samphillips1879@gmail.com>
2017-03-17 21:46:14 -0500

    simple memory detail view complete

---
+++ memory_trunk/memory_trunk_app/views/memory_detail_view.py
@@ -0,0 +1,19 @@
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from memory_trunk_app import models
+
+class MemoryDetailView(TemplateView):
+    """
+    Purpose: 
+        Display the detaiils of a specific Memory object
+    
+    Author: Sam Phillips <samcphillips.com>
+    """
+    template_name = 'memory_detail.html'
+
+    def get(self, request, id):
+        memory = models.Memory.objects.get(id=id)
+
+
+        return render(request, self.template_name, {'memory': memory})
