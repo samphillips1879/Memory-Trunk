f960f8c66ee845c2010e18926fe94792ac7a8d6a
Samuel Phillips <samphillips1879@gmail.com>
2017-03-18 09:18:11 -0500

    memory detail complete with tests and private check

--- memory_trunk/memory_trunk_app/views/memory_detail_view.py
+++ memory_trunk/memory_trunk_app/views/memory_detail_view.py
@@ -1,6 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from memory_trunk_app import models
 
 class MemoryDetailView(TemplateView):
@@ -14,6 +15,7 @@ class MemoryDetailView(TemplateView):
 
     def get(self, request, id):
         memory = models.Memory.objects.get(id=id)
-
-
+        if memory.is_public == False:
+            if memory.user != request.user:
+                return HttpResponseRedirect(redirect_to='/')
         return render(request, self.template_name, {'memory': memory})
