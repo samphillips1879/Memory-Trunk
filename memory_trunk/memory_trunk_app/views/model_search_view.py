12a92fbbc25db17350b72c71360a7b56ede5b53e
Samuel Phillips <samphillips1879@gmail.com>
2017-03-22 23:23:06 -0500

    search bar works but is ugly

--- memory_trunk/memory_trunk_app/views/model_search_view.py
+++ memory_trunk/memory_trunk_app/views/model_search_view.py
@@ -7,8 +7,9 @@ from django.urls import reverse
 def search(request, user=None):
     """
     Purpose:
-        Act as the switchboard operator for directing search requests
-        from a User-submitted query
+        Searches the MemoryTrunk database for model instances containing
+        a User's query -- either in the object's title, its content, 
+        or in its tags (case sensitive) -- and returns all matching instances rendered to their relative model list html files.
 
     Author: Sam Phillips <samcphillips.com>
     """
@@ -19,22 +20,28 @@ def search(request, user=None):
     template_name = ''
     ObjClass = None
     model_name = ''
+    page_title = ''
     if model == 'Memory':
         template_name = 'memory_list.html'
         ObjClass = models.Memory
         model_name = 'memories'
+        page_title = 'Memory Results'
     elif model == 'Tip':
         template_name = 'tip_list.html'
         ObjClass = models.Tip
         model_name = 'tips'
+        page_title = 'Tip Results'
     elif model == 'Perspective':
         template_name = 'perspective_list.html'
         ObjClass = models.Perspective
         model_name = 'perspectives'
+        page_title = 'Perspective Results'
 
     objects = ObjClass.objects.filter(content__icontains=query) | ObjClass.objects.filter(title__icontains=query) | ObjClass.objects.filter(tags__name__in=[query])
+    objects = set(objects)
 
     context[model_name] = objects
+    context['page_title'] = page_title
 
     return render(
         request,
